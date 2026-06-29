7b24138c52c617e5251defa11e2603e6f1e96f92
Brittany Price <bprice0496@gmail.com>
2026-06-28 21:03:47 -0400

    Pandas 03 done

--- assignments_01/warmup_01.py
+++ assignments_01/warmup_01.py
@@ -15,3 +15,8 @@ print(f"Column data types: {df.info()}")
 # Pandas 02
 df1 = df[(df['grade'] > 80)]
 print(f"Pandas Q2:\n {df1}")
+
+# Pandas 03
+df2 = df.copy()
+df2["grade_curved"] = df[["grade"]].apply(lambda x: x["grade"] + 5,axis=1)
+print(f"Pandas Q3:\n {df2}")
