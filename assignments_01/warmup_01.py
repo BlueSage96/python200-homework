bd1067429d5fdd4ad3563fccc4b47c9ac7264e75
Brittany Price <bprice0496@gmail.com>
2026-06-28 20:19:07 -0400

    Pandas 01 done

---
+++ assignments_01/warmup_01.py
@@ -0,0 +1,13 @@
+# Pandas 01
+import pandas as pd
+data = {
+    "name": ["Alice","Bob","Carol","David","Eve"],
+    "grade": [85,72,90,68,95],
+    "city": ["Boston","Austin","Boston","Denver","Austin"],
+    "passed": [True, True, True, False, True]
+}
+
+df = pd.DataFrame(data)
+print(f"First three rows: {df.head(3)}")
+print(f"Shape:{df.shape}")
+print(f"Column data types: {df.info()}")
