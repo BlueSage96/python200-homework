e023f21838573e30c620c586a6218f8d2e0bd4a5
Brittany Price <bprice0496@gmail.com>
2026-07-07 17:09:36 -0400

    Task 2 done

--- assignments_01/project_01.py
+++ assignments_01/project_01.py
@@ -18,12 +18,13 @@ paths = [
 
 
 # make function and use @task
-@task(retries=3,retry_delay_seconds=2)
+# @task(retries=3,retry_delay_seconds=2)
 def happiness_data():
     final_dataframe = []          
 
     for path in paths:
         read_path = pd.read_csv(path, sep=";")
+        read_path = read_path.rename(columns={"Ladder score":"Happiness score"})
         loop_path = pd.DataFrame(read_path)
         path_year = path.replace(".csv", "").rsplit("_")
 
@@ -32,7 +33,31 @@ def happiness_data():
         
     # merge all info into one csv --> go's to output folder
     happiness_merged = pd.concat(final_dataframe)
-    print(happiness_merged)
     happiness_merged.to_csv("outputs/merged_happiness.csv",index=False)
 happiness_data()
 
+data = pd.read_csv("outputs/merged_happiness.csv")
+df = pd.DataFrame(data)
+
+# Task 2
+#@task(retries=3,retry_delay_seconds=2)
+def happy_stats():
+    # A little cleanup
+    df.rename(columns={"Happiness score":"happiness_score","Regional indicator":"regional_indicator"},inplace=True)
+
+    df["happiness_score"] = df["happiness_score"].str.replace(",",".")
+    df["happiness_score"] = df["happiness_score"].astype(float).round(2)
+
+    happy_score = df["happiness_score"]
+    happy_mean = happy_score.mean()
+    happy_median = happy_score.median()
+    happy_std = happy_score.std()
+    
+    # close but I need to include happiness_score and .mean() -- look up examples!
+    happy_mean_grouped = df.groupby(["Year","regional_indicator"])["happiness_score"].mean()
+    
+    print(f"Mean:\n {happy_mean}")
+    print(f"\nMedian:\n {happy_median}")
+    print(f"\nStandard deviation:\n {happy_std}")
+    print(f"\nGrouped mean:\n {happy_mean_grouped}")
+happy_stats()
