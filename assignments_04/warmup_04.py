22e4883bcd30b86978256e279bf70f52dcddc631
Brittany Price <bprice0496@gmail.com>
2026-07-26 01:47:20 -0400

    ROC Question 01 done

--- assignments_04/warmup_04.py
+++ assignments_04/warmup_04.py
@@ -31,3 +31,26 @@ X, y = make_classification(
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42, stratify=y
 )
+
+#Scaled data
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.fit_transform(X_test)
+
+#ROC 01
+log_reg = LogisticRegression(max_iter=1000,random_state=42)
+log_reg.fit(X_train,y_train)
+y_probs = log_reg.predict_proba(X_test)[:,1]
+auc = roc_auc_score(y_test,y_probs)
+
+print(f"AUC not scaled: {auc:.3f}")
+
+knn = KNeighborsClassifier(n_neighbors=5)
+knn.fit(X_train_scaled,y_train)
+y_probs_scaled = knn.predict_proba(X_test_scaled)[:,1]
+auc_scaled = roc_auc_score(y_test,y_probs_scaled)
+
+print(f"AUC scaled: {auc_scaled:.3f}")
+
+#KNN has the better auc score meaning it has a better-discriminating 
+# model than the logistic regression.
