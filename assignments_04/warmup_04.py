2eca34c34662a0a1c5c43e34cb232590f3f5963d
Brittany Price <bprice0496@gmail.com>
2026-07-26 02:26:59 -0400

    ROC Question 02 done

--- assignments_04/warmup_04.py
+++ assignments_04/warmup_04.py
@@ -40,17 +40,36 @@ X_test_scaled = scaler.fit_transform(X_test)
 #ROC 01
 log_reg = LogisticRegression(max_iter=1000,random_state=42)
 log_reg.fit(X_train,y_train)
-y_probs = log_reg.predict_proba(X_test)[:,1]
-auc = roc_auc_score(y_test,y_probs)
+probs = log_reg.predict_proba(X_test)[:,1]
+auc = roc_auc_score(y_test,probs)
 
 print(f"AUC not scaled: {auc:.3f}")
 
 knn = KNeighborsClassifier(n_neighbors=5)
 knn.fit(X_train_scaled,y_train)
-y_probs_scaled = knn.predict_proba(X_test_scaled)[:,1]
-auc_scaled = roc_auc_score(y_test,y_probs_scaled)
+knn_probs = knn.predict_proba(X_test_scaled)[:,1]
+knn_auc = roc_auc_score(y_test,knn_probs)
 
-print(f"AUC scaled: {auc_scaled:.3f}")
+print(f"AUC scaled: {knn_auc:.3f}")
 
 #KNN has the better auc score meaning it has a better-discriminating 
 # model than the logistic regression.
+
+#ROC 02
+fpr, tpr, _ = roc_curve(y_test,probs)
+knn_fpr, knn_tpr, _ = roc_curve(y_test,knn_probs)
+
+fig, ax = plt.subplots(figsize=(6, 5))
+RocCurveDisplay(fpr=fpr,tpr=tpr).plot(ax=ax, name=f"Logistic Regression (AUC={auc:.2f})")
+RocCurveDisplay(fpr=knn_fpr, tpr=knn_tpr).plot(ax=ax, name=f"KNN k=5 (AUC={knn_auc:.2f})")
+
+ax.plot([0, 1], [0, 1], linestyle="--", color="gray", label="Random")
+ax.set_title("KNN and Linear Regression ROC Comparison")
+ax.legend()
+
+plt.tight_layout()
+plt.savefig("outputs/roc_comparison.png")
+plt.show()
+
+#1. KNN has the lower FPR when each model reaches TPS = 0.80.
+#2. KNN would produce fewer alarms when needing to catch 80% of positives.
