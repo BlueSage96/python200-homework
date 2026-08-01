79c546847b7726d05f38b4ab4873a59c933c1d33
Brittany Price <bprice0496@gmail.com>
2026-08-01 18:30:06 -0400

    API Question 02 done

--- assignments_05/warmup_05.py
+++ assignments_05/warmup_05.py
@@ -14,3 +14,35 @@ print(f"API 01:\n")
 print(f"Response: \n{response.choices[0].message.content}")
 print(f"\nModel: {response.model}")
 print(f"\nTokens used:\n{response.usage}")
+
+#API 02
+print(f"\nAPI 02:\n")
+
+prompt = "Suggest a creative name for a data engineering consultancy"
+temperatures = [0,0.7,1.5]
+
+response_temp1 = client.chat.completions.create(
+    model = "gpt-4o-mini",
+    messages=[{"role": "user","content":prompt}],
+    temperature=temperatures[0]
+)
+print(f"\nTemperature 1:\n{response_temp1.choices[0].message.content}")
+
+response_temp2 = client.chat.completions.create(
+    model = "gpt-4o-mini",
+    messages=[{"role": "user","content":prompt}],
+    temperature=temperatures[1]
+)
+print(f"\nTemperature 2:\n{response_temp2.choices[0].message.content}")
+
+response_temp3 = client.chat.completions.create(
+    model = "gpt-4o-mini",
+    messages=[{"role": "user","content":prompt}],
+    temperature=temperatures[2]
+)
+print(f"\nTemperature 3:\n{response_temp3.choices[0].message.content}")
+
+#1. Each time the code is ran, each response is different from one another 
+#   and different from the response of the run before
+#2. I would choose the 1.5 temperature as it has produced the best names
+#   has the most variation each run.
