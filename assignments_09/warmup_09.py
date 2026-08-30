68e2752856ba989181a42e661c87fa117e372528
Brittany Price <bprice0496@gmail.com>
2026-08-30 00:46:28 -0400

    CRUD 01 done

--- assignments_09/warmup_09.py
+++ assignments_09/warmup_09.py
@@ -29,8 +29,6 @@ def get_client():
     
     return supabase
 
-get_client()
-
 # Connection 03
 
 # Row Level Security (RLS) dictates what table rows a use can access. 
@@ -39,3 +37,24 @@ get_client()
 # A real-world use case would be making an important distinction between the admin and 
 # regular users in an app. The admin can access all of the data in the app while 
 # regular users can only see the data they created.
+
+# CRUD 01
+
+def insert_test_record(supabase):
+    record = {
+        "date":"2026-08-30",
+        "temperature_2m_max": 16.3,
+        "temperature_2m_min": 2.1,
+        "precipitation_sum":  0.7,
+        "wind_speed_10m_max": 15.5,
+    }
+    
+    response = supabase.table("weather_raw").insert(record).execute()
+    print(response.data)
+
+supabase = get_client()
+insert_test_record(supabase)
+
+# Running the function twice creates a 'duplicate key' error.
+# Would use "upsert" in the function instead of "insert" to 
+# make repeated calls safe.
