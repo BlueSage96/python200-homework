c7b98ac1ffadd211b4d2f687cab5ce64f61e4dec
Brittany Price <bprice0496@gmail.com>
2026-08-29 21:12:28 -0400

    Connection 01 done

---
+++ assignments_09/warmup_09.py
@@ -0,0 +1,14 @@
+
+
+# Connection 01
+
+# 1. supabase-py needs the supabase url and key to connect to the project.
+
+# 2. Both the supabase url can be accessed by hovering over the settings (gear icon) 
+#    and selecting "Project Settings". The url is the "Project ID".
+
+#    The supabase key can be found by clicking on "API keys" on the project's 
+#    main dashboard under the "Get Connected".
+
+#   We should never commit a key to a public GitHub repository because it can be scraped 
+#   within minutes by automated bots. 
