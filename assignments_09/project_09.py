e8f8748713e934784de2239d1ddc0cd6e86821c4
Brittany Price <bprice0496@gmail.com>
2026-08-30 05:29:25 -0400

    Step 01 done

---
+++ assignments_09/project_09.py
@@ -0,0 +1,23 @@
+import requests
+
+# Step 01
+print(f"\nStep 01:\n")
+
+url = "https://archive-api.open-meteo.com/v1/archive"
+params = {
+    "latitude": 34.99713980841658,
+    "longitude": -78.33071903597848,
+    "start_date": "2023-01-01",
+    "end_date": "2023-12-31",
+    "daily": [
+        "temperature_2m_max",
+        "temperature_2m_min",
+        "precipitation_sum",
+        "wind_speed_10m_max",
+    ],
+    "timezone": "America/New_York",
+}
+response = requests.get(url, params=params)
+response.raise_for_status()
+data = response.json()
+print(f"{data}\n")
