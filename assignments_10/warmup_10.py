dbacb74becb54a94a20838c8aace8c5a94a2a7d2
Brittany Price <bprice0496@gmail.com>
2026-09-06 01:00:18 -0400

    ML/LLM Question 01 done

---
+++ assignments_10/warmup_10.py
@@ -0,0 +1,20 @@
+
+
+# --- ML/LLM Questions ---
+
+# ML/LLM 01
+
+"""
+The ML classifier is best at fast, predictable predictions from the 
+structured data. For example, the ML classier can make predictions
+for good_for_running and confidence.
+
+The LLM is best at language, explanations, and judgment. It can
+be used to make human readable predictions such as, "Classify the sentiment. 
+Reply with exactly one word: positive, negative, or neutral."
+
+If the LLM was used to make predictions, it is possible that the data
+would unreadable to the model or even corrupted. If the ML was used 
+to make prompts, the data would be unreadable to a human or it gives
+inaccurate statements making it harder to properly train the model.
+"""
