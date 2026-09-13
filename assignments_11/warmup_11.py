2924af035d83f08d86c21fc4fcfd539630c39d57
Brittany Price <bprice0496@gmail.com>
2026-09-13 19:12:20 -0400

    Prefect 01 done

---
+++ assignments_11/warmup_11.py
@@ -0,0 +1,9 @@
+from prefect import flow, task
+
+# Prefect Q1
+
+# Tasks are the smallest unit in a pipeline and is used for instance, loading datam etc.
+# Flows represents the higher-level workflow logic that connects multiple tasks together 
+# into a whole pipeline. 
+# I would decorate the temperature conversion function with @task because it is a helper 
+# function and one piece in a pipeline.
